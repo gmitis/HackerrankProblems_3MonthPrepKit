7d02abde53eacda495e4cef22395f25db0cb9b43
gmitis <gi.muths@gmail.com>
2025-05-02 16:56:49 +0300

    Sparse Arrays: solved n^2

--- week1/SparseArrays.cs
+++ week1/SparseArrays.cs
@@ -16,14 +16,31 @@ class Result
 {
 
     /*
-     * Complete the 'miniMaxSum' function below.
+     * Complete the 'matchingStrings' function below.
      *
-     * The function accepts INTEGER_ARRAY arr as parameter.
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. STRING_ARRAY strings
+     *  2. STRING_ARRAY queries
      */
 
-    public static void miniMaxSum(List<int> arr)
+    public static List<int> matchingStrings(List<string> strings, List<string> queries)
     {
+        List<int> queryMatch = new List<int>();
+        int counter;
 
+        foreach (var query in queries) 
+        {
+            counter = 0;
+            foreach (var str in strings) 
+            {
+                if (query == str) counter++;
+            }
+
+            queryMatch.Add(counter);
+        }
+
+        return queryMatch;
     }
 
 }
@@ -32,9 +49,33 @@ class Solution
 {
     public static void Main(string[] args)
     {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
-        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+        int stringsCount = Convert.ToInt32(Console.ReadLine().Trim());
 
-        Result.miniMaxSum(arr);
+        List<string> strings = new List<string>();
+
+        for (int i = 0; i < stringsCount; i++)
+        {
+            string stringsItem = Console.ReadLine();
+            strings.Add(stringsItem);
+        }
+
+        int queriesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<string> queries = new List<string>();
+
+        for (int i = 0; i < queriesCount; i++)
+        {
+            string queriesItem = Console.ReadLine();
+            queries.Add(queriesItem);
+        }
+
+        List<int> res = Result.matchingStrings(strings, queries);
+
+        textWriter.WriteLine(String.Join("\n", res));
+
+        textWriter.Flush();
+        textWriter.Close();
     }
 }
